a83dce94b68bae37fc113bbd7edca1a1ee0e9815
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-12-02 13:55:54 +0100

    nahodne vygenerujeme jeden file do databazy

--- fishare/helpers.py
+++ fishare/helpers.py
@@ -1,8 +1,32 @@
+from random import choice, randint
 from models.file import File
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session
+from faker import Faker
 
 from database import engine
 
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
+
+
+def populate_data():
+    faker = Faker()
+
+    categories = ('audio', 'video', 'image')
+
+    category = choice(categories)
+
+    file = File(
+        filename=faker.file_name(category),
+        size=randint(10, 1000000),
+        mime_type=faker.mime_type(category)
+    )
+
+    print(repr(file))
+
+    session = Session(engine)
+    session.add(file)
+    session.commit()
+
+
