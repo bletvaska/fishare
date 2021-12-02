c62e20c073fc5bec111a12a7b28db820ea744fb2
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-12-02 11:52:28 +0100

    priprava databazy

---
+++ fishare/database.py
@@ -0,0 +1,7 @@
+from sqlmodel import create_engine
+
+from models.settings import Settings
+
+settings = Settings()
+
+engine = create_engine(settings.db_uri, echo=True)
--- fishare/models/settings.py
+++ fishare/models/settings.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings, AnyHttpUrl
 class Settings(BaseSettings):
     base_url: AnyHttpUrl = 'http://localhost:8080'
     slug_length: int = 5
+    db_uri: str = 'sqlite:///default.db'
 
     class Config:
         env_file = '.env'
