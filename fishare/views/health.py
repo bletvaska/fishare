df4e94a58675f77808dd0659651305c82f70131c
Miroslav Biňas <miroslav.binas@tuke.sk>
2021-12-08 01:21:37 +0100

    * urobena samostatna routa pre /health/
    * pridany readiness a liveness pre overenie zdravia sluzby

--- fishare/views/health.py
+++ fishare/views/health.py
@@ -3,21 +3,42 @@ from datetime import datetime
 from fastapi import APIRouter
 
 from fishare.models.settings import Settings
+from fishare.database import engine
 
 router = APIRouter()
 settings = Settings()
 
 
-@router.get('/health', summary='Returns microservice health check.')
+@router.get('/', summary='Returns microservice health check.')
 def get_health():
     return {
         'ts': datetime.now(),
+        'status': 'running',
+        'appVersion': 0.0,
+        'python': 0.0,
         'storage': {
             'path': settings.storage,
-            'usage': 0,
+            'used': 0,
+            'max': 0,
             'files': 0
         },
         'db': {
-            'uri': settings.db_uri
+            'uri': settings.db_uri,
         },
+    }
+
+
+@router.get('/ready', summary='Checks readiness.')
+def get_readiness():
+    return {
+        'ts': datetime.now(),
+        'status': 'ready'
+    }
+
+
+@router.get('/alive', summary='Checks liveness.')
+def get_liveness():
+    return {
+        'ts': datetime.now(),
+        'status': 'up'
     }
