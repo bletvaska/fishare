27309013079018aa3d245069830d0c82d1c5e3c6
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-04 14:25:16 +0200

    priprava na spustenie projektu

--- fishare/main.py
+++ fishare/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return "Hello, World!"
+
+
+def main():
+    uvicorn.run('fishare.main:app', port=9000, host='0.0.0.0', reload=False)
+
+
+if __name__ == '__main__':
+    main()
