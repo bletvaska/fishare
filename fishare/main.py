978962dbc3e5ba24a70072cf321dba62f141772e
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-04 15:04:49 +0200

    pociatok tvorby rest api pre fishare

--- fishare/main.py
+++ fishare/main.py
@@ -4,14 +4,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def hello():
-    return "Hello, World!"
+@app.get("/api/v1/files/")
+def get_list_of_files():
+    return [
+        "file1",
+        "file2",
+        "file3"
+    ]
+
+
+@app.get('/hello/{name}')
+def greetings(name: str):
+    return f'hello {name}'
 
 
 def main():
-    print('>> running')
-    uvicorn.run('fishare.main:app', port=9000, host='0.0.0.0', reload=False)
+    uvicorn.run('fishare.main:app', port=9000, host='0.0.0.0', reload=True)
 
 
 if __name__ == '__main__':
