94a87dc4c13f46fc5d3029539726f64b07f128af
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-04 15:14:55 +0200

    pridane dalsie endpointy

--- fishare/main.py
+++ fishare/main.py
@@ -25,6 +25,16 @@ def create_file():
     return 'file was created'
 
 
+@app.delete('/api/v1/files/{filename}')
+def delete_file(filename: str):
+    return f'file {filename} was deleted.'
+
+
+@app.put('/api/v1/files/{filename}')
+def full_update_file(filename: str):
+    return f'file {filename} is going to be fully updated'
+
+
 def main():
     uvicorn.run('fishare.main:app', port=9000, host='0.0.0.0', reload=True)
 
