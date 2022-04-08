eddb4557030cc60d43e725e2b82722cd28a3270b
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-08 14:26:24 +0200

    admin template 2

--- fishare/views/admin.py
+++ fishare/views/admin.py
@@ -1,8 +1,16 @@
 from fastapi import Request, APIRouter
+from fastapi.templating import Jinja2Templates
+
 
 router = APIRouter()
+templates = Jinja2Templates(directory='fishare/templates/')
 
 
 @router.get('/admin/')
 def list_of_files(request: Request):
-    return 'hello world'
+    data = {
+        'request': request,
+        'title': 'fishare Service'
+    }
+
+    return templates.TemplateResponse('admin.html', data)
