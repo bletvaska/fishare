2ac0dd10d577a7283b7ff4b8e1fee5f16250c7fb
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-08 14:32:52 +0200

    vytiahnuty zoznam vsetkych suborov z db pre admin

--- fishare/views/admin.py
+++ fishare/views/admin.py
@@ -1,16 +1,26 @@
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, Depends
 from fastapi.templating import Jinja2Templates
+from sqlmodel import Session, select
 
+from fishare.database import get_session
+from fishare.models.file import File
 
 router = APIRouter()
 templates = Jinja2Templates(directory='fishare/templates/')
 
 
 @router.get('/admin/')
-def list_of_files(request: Request):
+def list_of_files(request: Request, session: Session = Depends(get_session)):
+    # get list of all files
+    statement = select(File)
+    files = session.exec(statement).all()
+
+    # prepare template data
     data = {
         'request': request,
-        'title': 'fishare Service'
+        'title': 'fishare Admin',
+        'files': files
     }
 
+    # render
     return templates.TemplateResponse('admin.html', data)
