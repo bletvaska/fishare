5574a82e1c41f56bbc4ba18b5fd5ede53b95b8b0
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-04-08 11:12:50 +0200

    skoro hotovy endpoint /cron

--- fishare/api/cron.py
+++ fishare/api/cron.py
@@ -1,12 +1,45 @@
+from datetime import datetime
+
 import fastapi
 from fastapi import Depends
-from sqlmodel import Session
+from sqlalchemy import or_
+from sqlmodel import Session, select
 
 from fishare.database import get_session
+from fishare.models.file import File
+from fishare.models.settings import Settings
 
 router = fastapi.APIRouter()
+settings = Settings()
 
 
 @router.get("/", summary="Run background jobs.")
 def run_cron_job(session: Session = Depends(get_session)):
-    return 'running cron jobs.'
+    start = datetime.now()
+
+    # query
+    statement = select(File).where(or_(datetime.now() > File.expires, File.downloads >= File.max_downloads))
+    files = session.exec(statement).all()
+
+    # delete files
+    for file in files:
+        # prepare path
+        path = settings.storage / file.slug
+
+        # delete file
+        # session.delete(file)
+        # session.commit()
+
+        # delete file from storage
+        # path.unlink(missing_ok=True)
+
+    # count duration
+    end = datetime.now()
+    duration = end - start
+
+    return {
+        'startedAt': start,
+        'finishedAt': end,
+        'removedFiles': len(files),
+        'duration': duration.total_seconds()
+    }
