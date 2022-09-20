16a514a6aa6939bcd9e81d0b40f4f9ebee6d4fe8
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-20 10:16:59 +0200

    created output model for files

---
+++ fishare/models/file_details_out.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from pydantic import BaseModel, validator, HttpUrl
+
+
+class FileDetailsOut(BaseModel):
+    slug: str
+    filename: str
+    downloads = 0
+    max_downloads = 1
+    size: int
+    mime_type: str
+    url: HttpUrl = None
+
+    @validator('url', always=True)
+    def set_file_details_url(cls, value, values):
+        print('>> validating url')
+        print(values)
+        return f'http://localhost:8000/'
