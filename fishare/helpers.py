971348511958dd0b6bc3f073b2bce7734b653b8e
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-20 13:43:48 +0200

    added faker

---
+++ fishare/helpers.py
@@ -0,0 +1,27 @@
+from random import random
+
+from faker import Faker
+from sqlmodel import create_engine, Session
+
+from fishare.models.file_details import FileDetails
+from fishare.models.settings import get_settings
+
+
+def populate_data(count: int = 10):
+    engine = create_engine(get_settings().db_uri)
+
+    faker = Faker()
+    categories = ('audio', 'video', 'image', 'text')
+
+    with Session(engine) as session:
+        for _ in range(count):
+            category = random.choice(categories)
+            file = FileDetails(
+                id=_,
+                filename=faker.file_name(category=category),
+                mime_type=faker.mime_type(category=category),
+                size=random.randint(100, 100000000)
+            )
+            session.add(file)
+
+        session.commit()
