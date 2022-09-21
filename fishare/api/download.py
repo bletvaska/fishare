8be36f7d23e51cec1d2797acef4f8940e6bb2510
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-21 15:10:15 +0200

    downloading works

--- fishare/api/download.py
+++ fishare/api/download.py
@@ -3,22 +3,33 @@ from fastapi import Depends
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, FileResponse
 
 from fishare.database import get_session
 from fishare.models.file_details import FileDetails
 from fishare.models.problem_details import ProblemDetails
+from fishare.models.settings import get_settings, Settings
 
 router = fastapi.APIRouter()
 
 
-@router.get('/{slug}', summary='Download file by {slug}.')
+@router.get('/{slug}', status_code=200,
+            summary='Download file by {slug}.')
 def download_file(request: Request, slug: str,
-                  session: Session = Depends(get_session)):
+                  session: Session = Depends(get_session),
+                  settings: Settings = Depends(get_settings)):
     try:
         # get file by slug
         statement = select(FileDetails).where(FileDetails.slug == slug)
         file = session.exec(statement).one()
+
+        # return file
+        return FileResponse(
+            settings.storage / file.slug,   # path
+            media_type=file.mime_type,      # content-type
+            filename=file.filename          # filename
+        )
+
     except NoResultFound as ex:
         problem = ProblemDetails(
             status=404,
