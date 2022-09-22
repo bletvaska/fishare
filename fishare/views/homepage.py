518649103344f8df6f02cc3e5a6fabc0d9d26f97
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-22 13:57:39 +0200

    first html view created

--- fishare/views/homepage.py
+++ fishare/views/homepage.py
@@ -1,8 +1,13 @@
 import fastapi
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 router = fastapi.APIRouter()
+templates = Jinja2Templates(directory='fishare/templates/')
 
 
 @router.get('/')
-def homepage():
-    pass
+def homepage(request: Request):
+    return templates.TemplateResponse('home.html', {
+        'request': request,
+    })
