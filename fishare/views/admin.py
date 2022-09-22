d70c4ff8e4f80ec71b06f13cb5fd28c61727f6b0
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-22 14:47:00 +0200

    simple admin view

--- fishare/views/admin.py
+++ fishare/views/admin.py
@@ -1,20 +1,30 @@
 import fastapi
 from fastapi import Depends
+from sqlmodel import select, Session
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from fishare.database import get_session
 from fishare.helpers import get_jinja
+from fishare.models.file_details import FileDetails
 
 router = fastapi.APIRouter()
 
 
 @router.get('/')
 def list_of_files(request: Request,
-                  jinja: Jinja2Templates = Depends(get_jinja)):
+                  jinja: Jinja2Templates = Depends(get_jinja),
+                  session: Session = Depends(get_session)):
+    # SELECT * FROM files
+    statement = select(FileDetails)
+    files = session.exec(statement).all()
+
+    # prepare data
     data = {
         'request': request,
         'title': 'fishare - List of Files',
-        'files': []
+        'files': files
     }
 
+    # render
     return jinja.TemplateResponse('admin.html', data)
