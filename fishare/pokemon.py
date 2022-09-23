2d5f949849f79cfb819415e9ee0083b59eab39b7
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-23 11:13:25 +0200

    pokedex

---
+++ fishare/pokemon.py
@@ -0,0 +1,21 @@
+from dataclasses import Field
+
+from sqlmodel import SQLModel, create_engine, Session, select
+
+
+class Pokemon(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    pokedex_number: int
+    name: str
+    type1: str
+    type2: str
+
+
+db_uri = 'sqlite:////tmp/pokedex.sqlite'
+
+engine = create_engine(db_uri)
+with Session(engine) as session:
+    statement = select(Pokemon).limit(10)
+    pokemons = session.exec(statement).all()
+
+    print(pokemons)
