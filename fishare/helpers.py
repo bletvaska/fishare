09da6dbecbecf370f2feaee2baae4d5abc3be85a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-23 10:45:07 +0200

    refactoring

--- fishare/helpers.py
+++ fishare/helpers.py
@@ -1,13 +1,10 @@
 import random
-from functools import lru_cache
 
 from faker import Faker
 from sqlmodel import create_engine, Session
-from starlette.templating import Jinja2Templates
 
+from fishare.dependencies import get_settings
 from fishare.models.file_details import FileDetails
-from fishare.models.settings import get_settings
-
 
 
 def populate_data(count: int = 100):
