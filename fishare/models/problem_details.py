6be7d876c675d60e921eefb9bc72deab9dd285ac
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-29 07:35:03 +0100

    priprava na novy beh skolenia

--- fishare/models/problem_details.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class ProblemDetails(BaseModel):
-    type = "about:blank"
-    title: str
-    status: int | None
-    detail: str | None
-    instance: str | None
