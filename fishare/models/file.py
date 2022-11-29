5b6314bab660f7ce7cfafd66c9907b5f7ea8f803
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-29 14:00:09 +0100

    created model for File

--- fishare/models/file.py
+++ fishare/models/file.py
@@ -1,5 +1,16 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, HttpUrl
 
 
 class File(BaseModel):
-    pass
+    # id: int
+    slug: str | None = None
+    filename: str
+    url: HttpUrl | None = None
+    expires: datetime | None = None
+    downloads = 0
+    max_downloads = 1
+    size: int
+    mime_type: str = 'application/octet-stream'
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
