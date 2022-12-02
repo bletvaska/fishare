356ba4a8c31a4f85fbc8c08b7ffb52e7602a30e3
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-12-02 13:56:09 +0100

    creating healthcheck

--- fishare/api/healthcheck.py
+++ fishare/api/healthcheck.py
@@ -1,9 +1,31 @@
 import fastapi
+from sqlmodel import Session
+
+from fishare.dependencies import get_session, get_settings
+from fishare.models.settings import Settings
 
 
 router = fastapi.APIRouter()
 
 
+def is_db_session(session: Session):
+    return session is not None
+
+
+def check_storage_state(settings: Settings):
+    return settings.storage.exists() and settings.storage.is_dir()
+
+
 @router.get("/healthz")
-def healthcheck():
-    pass
+def healthcheck(
+    session: Session = fastapi.Depends(get_session),
+    settings: Settings = fastapi.Depends(get_settings),
+):
+    db_state = is_db_session(session)
+    fs_state = check_storage_state(settings)
+
+    return {
+        "healthy": db_state and fs_state,
+        "dbState": db_state,
+        "fsState": fs_state
+        }
